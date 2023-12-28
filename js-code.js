6c3d60f2e8100789a42ed2039c6057595e852a52
LHazlebeck <122494690+LHazlebeck@users.noreply.github.com>
2023-12-27 19:49:37 -0500

    Add files via upload

---
+++ js-code.js
@@ -0,0 +1,225 @@
+const container = document.querySelector('#centeredmenu');
+const body = document.querySelector('body')
+const html = document.querySelector('html')
+const brug = document.querySelector('.brug')
+
+
+const plant = new Audio('./sounds/c4plant.mp3');
+const beep = new Audio('./sounds/c4Sound.mp3');
+const defuse = new Audio('./sounds/c4defuse.mp3')
+const tWin = new Audio('./sounds/tWin.mp3')
+const csWin = new Audio('./sounds/csWin.mp3')
+const defuseAnnounce = new Audio('./sounds/defuseAnnouncement.mp3')
+const yay = new Audio('./sounds/yayyyyy.mp3')
+let jerry;
+defuseAnnounce.addEventListener('ended', (event) => {
+    csWin.play()
+    jerry = setTimeout(yay.play(), 3000)
+  });
+
+var active = false;
+var timeCheck = false;
+
+function refresh() {
+    tWin.pause()
+    tWin.currentTime = 0
+    beep.pause()
+    beep.currentTime = 0
+    csWin.pause()
+    csWin.currentTime = 0
+    yay.pause()
+    yay.currentTime = 0
+    defuseAnnounce.pause()
+    defuseAnnounce.currentTime = 0
+    clearTimeout(jerry)
+
+    html.style.backgroundColor = 'black'
+    body.style.backgroundColor = 'black'
+    body.style.justifyContent = 'center'
+    body.innerHTML = ""
+    bombSet()
+}
+
+function defuseSet() {
+    html.style.backgroundColor = 'orange'
+    body.style.backgroundColor = 'orange'
+    let button = document.createElement('button');
+    button.style.backgroundColor = '#0649BD'
+    let plantIcon = document.createElement('img')
+    plantIcon.src = "./Defuser_hud_csgoa.png"
+    plantIcon.setAttribute('style', 'height: 7rem; width:fit-content; filter:brightness(0%)');
+    body.appendChild(button);
+    button.appendChild(plantIcon);
+
+    let notExploded = true;
+
+    function defused() {
+        clearTimeout(countdown)
+        beep.pause()
+        beep.currentTime = 0
+        html.style.backgroundColor = '#23ff0a'
+        body.style.backgroundColor = '#23ff0a'
+        defuseAnnounce.play()
+
+        body.style.gap = '2rem'
+        body.style.padding = '3rem'
+
+        let text = document.createElement('b')
+        text.textContent = "Counter Terrorists Win"
+        text.style.color = "white"
+        text.style.fontSize = "1.2 rem"
+
+        let button2 = document.createElement('button');
+        button2.textContent = "Refresh"
+        button2.setAttribute('style', 'padding: 2rem; font-size: 1.25rem;padding-left: 4rem;border-radius: 10%;padding-right: 4rem; background-color: white; width: 3rem; height: 2rem; color: #FF0F0F')
+
+        button.remove()
+        body.appendChild(text)
+        body.appendChild(button2)
+
+        button2.addEventListener("touchstart", function(){
+            refresh()
+        })
+        button2.addEventListener("click", function(){
+            refresh()
+        })
+    }
+
+    function explosion() {
+        clearTimeout(plantTimer);
+        html.style.backgroundColor = '#FF0F0F'
+        body.style.backgroundColor = '#FF0F0F'
+        body.style.justifyContent = 'space-around'
+        body.style.padding = '3rem'
+        let text = document.createElement('b')
+        text.textContent = "Terrorists Win"
+
+        let skull = document.createElement('img')
+        skull.src = "./skull-danger-png.png"
+        skull.setAttribute('style', 'height: 8rem; width:fit-content;')
+
+        let button2 = document.createElement('button');
+        button2.textContent = "Refresh"
+        button2.setAttribute('style', 'padding: 2rem; font-size: 1.25rem;padding-left: 4rem;border-radius: 10%;padding-right: 4rem; background-color: black; width: 3rem; height: 2rem; color: #FF0F0F')
+
+        button.remove()
+        body.appendChild(text)
+        body.appendChild(skull)
+        body.appendChild(button2)
+
+        button2.addEventListener("touchstart", function(){
+            refresh()
+        })
+        button2.addEventListener("click", function(){
+            refresh()
+        })
+
+        setTimeout(tWin.play(), 1000)
+    }
+    
+    button.addEventListener("touchstart", function(){
+        if (active) return;
+        defuse.currentTime = 0;
+        defuse.play();
+        button.style.backgroundColor = '#32D60D'
+        plantIcon.setAttribute('style', 'height: 7rem; width:fit-content; filter:brightness(100%)');
+        plantTimer = window.setTimeout(defused, 5000);
+    })
+
+    button.addEventListener("mousedown", function(){
+        if (active) return;
+        defuse.currentTime = 0;
+        defuse.play();
+        button.style.backgroundColor = '#32D60D'
+        plantIcon.setAttribute('style', 'height: 7rem; width:fit-content; filter:brightness(100%)');
+        plantTimer = window.setTimeout(defused, 5000);
+    })
+    
+    button.addEventListener("touchend", function(){
+       // plant.pause();
+        if (active) return;
+        button.style.backgroundColor = '#0649BD'
+        plantIcon.setAttribute('style', 'height: 7rem; width:fit-content; filter:brightness(0%)');
+        clearTimeout(plantTimer);
+    })
+
+    button.addEventListener("mouseup", function(){
+        // plant.pause();
+         if (active) return;
+         button.style.backgroundColor = '#0649BD'
+         plantIcon.setAttribute('style', 'height: 7rem; width:fit-content; filter:brightness(0%)');
+         clearTimeout(plantTimer);
+     })
+ //   html.style.backgroundColor = '#FF0F0F'
+
+ //   let start = Date.now()
+ //   let fComplete = (Date.now() - start) /40;
+  //  let freq = Math.max( 0.1 + 0.9 * fComplete, 0.15 );
+    let countdown = window.setTimeout(explosion, 45850);
+
+ /*    setInterval(function() {
+        html.classList.add(brug);
+        fComplete = (Date.now() - start) /40;
+        freq = Math.max( 0.1 + 0.9 * fComplete, 0.15 );
+        anim = freq * 1000
+        console.log(freq)
+    }, freq * 1000) */
+    
+}
+
+function bombSet() {
+    tWin.pause()
+    tWin.currentTime = 0
+    beep.pause()
+    beep.currentTime = 0
+    let button = document.createElement('button');
+    let plantIcon = document.createElement('img')
+    plantIcon.src = "./C4_hud_csgo.png"
+    plantIcon.setAttribute('style', 'height: 8rem; width:fit-content; filter:brightness(0%)');
+    body.appendChild(button);
+    button.appendChild(plantIcon);
+
+    function planted() {
+        button.remove();
+        defuseSet();
+        beep.play();
+    }
+    
+    button.addEventListener("touchstart", function(){
+        if (active) return;
+        plant.currentTime = 0;
+        plant.play();
+        button.style.backgroundColor = '#FF0F0F'
+        plantIcon.setAttribute('style', 'height: 8rem; width:fit-content; filter:brightness(100%)');
+        plantTimer = window.setTimeout(planted, 3000);
+    })
+
+    button.addEventListener("mousedown", function(){
+        if (active) return;
+        plant.currentTime = 0;
+        plant.play();
+        button.style.backgroundColor = '#FF0F0F'
+        plantIcon.setAttribute('style', 'height: 8rem; width:fit-content; filter:brightness(100%)');
+        plantTimer = window.setTimeout(planted, 3000);
+    })
+    
+    button.addEventListener("touchend", function(){
+       // plant.pause();
+        if (active) return;
+        button.style.backgroundColor = '#eaee1f'
+        plantIcon.setAttribute('style', 'height: 8rem; width:fit-content; filter:brightness(0%)');
+        clearTimeout(plantTimer);
+    })
+
+    button.addEventListener("mouseup", function(){
+        // plant.pause();
+         if (active) return;
+         button.style.backgroundColor = '#eaee1f'
+         plantIcon.setAttribute('style', 'height: 8rem; width:fit-content; filter:brightness(0%)');
+         clearTimeout(plantTimer);
+     })
+}
+
+
+bombSet()
+
